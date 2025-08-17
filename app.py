d51b17a888693a2a808ce520ed95406f75ac9671
30November <biswayanmal@gmail.com>
2025-08-17 23:19:05 +0530

    Necessary resource added

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-print("Hello World")
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def library():
+    return render_template("library.html")
+
+@app.route("/student")
+def student():
+    return render_template("student.html")
+
+@app.route("/staff")
+def staff():
+    return render_template("staff.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+    
+@app.route("/abtus")
+def About_Us():
+    return render_template("abtus.html")
+
+@app.route("/cnt")
+def contact():
+    return render_template("cnt.html")
+    
+
+if __name__== "__main__":
+    app.run(debug=True)
