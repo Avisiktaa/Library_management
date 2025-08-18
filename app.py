9b65aa93b90aacfb47efea312304445dcaad30fa
30November <biswayanmal@gmail.com>
2025-08-18 23:00:10 +0530

    39

--- app.py
+++ app.py
@@ -31,7 +31,6 @@ class Staff(db.Model):# staff add
 class Student(db.Model):# staff add
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50))
-    password = db.Column(db.String(50))
 
     
 
