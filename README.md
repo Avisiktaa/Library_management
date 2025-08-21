1cb4c170cf2963f6eb114b953228830ee3d4da8f
30November <biswayanmal@gmail.com>
2025-08-21 20:57:35 +0530

    39

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Project Team name:<br>
 Biswayan 39 <br>
 Avisikta 47 <br>
+
+
+*Work*
+Biswayan
+1. 21 Aug, worked on Add and Delete Employee 
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 from datetime import datetime
@@ -15,7 +15,6 @@ class Book(db.Model):# for book entry
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(50))
     author = db.Column(db.String(50))
-    price = db.Column(db.Integer)
     user = db.Column(db.String(10),default=None)
     date = db.Column(db.Date,default=None)
 
@@ -28,11 +27,13 @@ class Stock(db.Model): #student will search the book
 class Staff(db.Model):# staff add
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+    mail = db.Column(db.String(50))
+    # password = db.Column(db.String(50))   for simplicity -> halt
 
 class Student(db.Model):# student add
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50))
+    year = db.Column(db.Date,default=(datetime.now().year + 1)) 
 
 #useful function
 def id_g(cls,name,role):
@@ -64,10 +65,28 @@ def student():
 def staff():
     return render_template("staff.html")
 
-@app.route("/admin")
+@app.route("/admin",methods=["GET","POST"])
 def admin():
-    return render_template("admin.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        mail = request.form["mail"]
+        emp_add = Staff(name=name,mail=mail)
+        db.session.add(emp_add)
+        db.session.commit()
+        return redirect("/admin")
     
+    return render_template("admin.html",emp = Staff.query.all())
+
+
+    
+@app.route("/edel/<int:no>",methods=["POST","GET"])
+def Employee_Del(no):
+    emp_fetch = Staff.query.get(no)
+    db.session.delete(emp_fetch)
+    db.session.commit()
+    return redirect("/admin")
+
+
 @app.route("/abtus")
 def About_Us():
     return render_template("abtus.html")
@@ -78,5 +97,5 @@ def contact():
     
 
 if __name__== "__main__":
-    #with app.app_context(): db.create_all()
+    with app.app_context(): db.create_all()
     app.run(debug=True)
